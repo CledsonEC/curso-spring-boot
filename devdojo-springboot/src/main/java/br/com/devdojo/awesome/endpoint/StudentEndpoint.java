502c185f3c9adea3d0056b549b038978a5b94f9d
Cledson <cledson.caetano@gmail.com>
2018-11-23 22:11:20 -0200

    Aula 05 - Criando o método de recuperar por Id

--- devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -5,26 +5,42 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.devdojo.awesome.error.CustomErrorType;
 import br.com.devdojo.awesome.model.Student;
 import br.com.devdojo.awesome.util.DateUtil;
 
 @RestController
-@RequestMapping("student")
+@RequestMapping("students")
 public class StudentEndpoint {
 
 	@Autowired
 	private DateUtil dateUtil;
 	
-	@RequestMapping(method = RequestMethod.GET, path = "/list")
-	public List<Student>listAll(){
+	public StudentEndpoint(DateUtil dateUtil) {
+		this.dateUtil = dateUtil;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<?> listAll(){
+				
+		return new ResponseEntity<>(Student.studentList,HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path ="/{id}")
+	public ResponseEntity<?> getStudentById(@PathVariable("id") int id){
+		Student student = new Student();
+		student.setId(id);
+		int index = Student.studentList.indexOf(student);
 		
-		System.out.println("------"+dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-		
-		
-		return Arrays.asList(new Student( "Deku"), new Student("Blah"));
+		if(index == -1)
+			return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);		
+		return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
 	}
 }
