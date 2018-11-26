f333c797b7ce21c957909c586fe650b9b988ddba
Cledson <cledson.caetano@gmail.com>
2018-11-26 21:51:21 -0200

    Aula 07 - Outro modo de fazer o mapeamento dos verbos http no crud

--- devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -7,7 +7,11 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,12 +32,12 @@ public class StudentEndpoint {
 		this.dateUtil = dateUtil;
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<?> listAll(){
 		return new ResponseEntity<>(Student.studentList,HttpStatus.OK);
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, path ="/{id}")
+	@GetMapping( path ="/{id}")
 	public ResponseEntity<?> getStudentById(@PathVariable("id") int id){
 		Student student = new Student();
 		student.setId(id);
@@ -44,19 +48,19 @@ public class StudentEndpoint {
 		return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
 	public ResponseEntity<?> save(@RequestBody Student student){
 		Student.studentList.add(student);
 		return new ResponseEntity<>(student, HttpStatus.OK);
 	}
 	
-	@RequestMapping(method = RequestMethod.DELETE)
+	@DeleteMapping
 	public ResponseEntity<?> delete(@RequestBody Student student){
 		Student.studentList.remove(student);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
-	@RequestMapping(method = RequestMethod.PUT)
+	@PutMapping
 	public ResponseEntity<?> update(@RequestBody Student student){
 		Student.studentList.remove(student);
 		Student.studentList.add(student);
