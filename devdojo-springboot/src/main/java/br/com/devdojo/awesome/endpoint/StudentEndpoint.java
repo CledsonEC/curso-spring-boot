1027bdab061c04ad8b6a550d37abd4ff89b0a32d
Cledson <cledson.caetano@gmail.com>
2018-12-06 22:30:19 -0200

    Aula 19 - Tratando permissão de autenticação com padrão nas urls com AntMatcher

--- devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -26,7 +26,7 @@ import br.com.devdojo.awesome.model.Student;
 import br.com.devdojo.awesome.repository.StudentRepository;
 
 @RestController
-@RequestMapping("students")
+@RequestMapping("v1")
 public class StudentEndpoint {
 
 	private final StudentRepository studentDAO;
@@ -36,12 +36,12 @@ public class StudentEndpoint {
 		this.studentDAO = studentDAOl;
 	}
 
-	@GetMapping
+	@GetMapping(path = "protected/students")
 	public ResponseEntity<?> listAll(Pageable pageable) {
 		return new ResponseEntity<>(studentDAO.findAll(pageable), HttpStatus.OK);
 	}
 
-	@GetMapping(path = "/{id}")
+	@GetMapping(path = "protected/students/{id}")
 	public ResponseEntity<?> getStudentById(@PathVariable("id") long id,
 				@AuthenticationPrincipal UserDetails userDetails) {
 		verifyIfStudentExists(id);
@@ -50,26 +50,26 @@ public class StudentEndpoint {
 		return new ResponseEntity<>(student, HttpStatus.OK);
 	}
 
-	@GetMapping(path = "/findByName/{name}")
+	@GetMapping(path = "protected/students/findByName/{name}")
 	public ResponseEntity<?> findStudentByName(@PathVariable String name) {
 		return new ResponseEntity<>(studentDAO.findByNameIgnoreCaseContaining(name), HttpStatus.OK);
 	}
 
-	@PostMapping
+	@PostMapping (path = "admin/students")
 	@Transactional //Caso tenha alguma excessão não será feiro o commit
 	public ResponseEntity<?> save(@Valid @RequestBody Student student) {
 		return new ResponseEntity<>(studentDAO.save(student), HttpStatus.CREATED);
 	}
 	
-	@DeleteMapping(path = "/{id}")
-	@PreAuthorize("hasRole('ADMIN')")
+	@DeleteMapping(path = "admin/students/{id}")
+	//@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<?> delete(@PathVariable long id) {
 		verifyIfStudentExists(id);
 		studentDAO.deleteById(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
-	@PutMapping
+	@PutMapping (path = "admin/students")
 	public ResponseEntity<?> update(@RequestBody Student student) {
 		verifyIfStudentExists(student.getId());
 		studentDAO.save(student);
