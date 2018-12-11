477bf1a317f780ae88d748b8fd8cfa9ec8eabe49
Cledson <cledson.caetano@gmail.com>
2018-12-10 22:17:03 -0200

    Aula 22 e Aula 23 - Consumindo rest api com spring boot (POST e GET)

---
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/javaclient/javaClientDAO.java
@@ -0,0 +1,54 @@
+package br.com.devdojo.awesome.javaclient;
+
+import java.util.List;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.devdojo.awesome.model.PageableResponse;
+import br.com.devdojo.awesome.model.Student;
+
+public class javaClientDAO {
+	private RestTemplate restTemplate = new RestTemplateBuilder()
+			.rootUri("http://localhost:8081/v1/protected/students")
+			.basicAuthentication("cledson", "devdojo")
+			.build();
+	
+	private RestTemplate restTemplateAdmin = new RestTemplateBuilder()
+			.rootUri("http://localhost:8081/v1/admin/students")
+			.basicAuthentication("cledson", "devdojo")
+			.build();
+	
+	
+	public Student findById(long id) {
+		Student student = restTemplate.getForObject("/{id}", Student.class,id);
+		//ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class,1);
+		return student;
+	}
+	
+	public List<Student> listAll(){
+		ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
+                new ParameterizedTypeReference<PageableResponse<Student>>() {
+                });
+        return exchange.getBody().getContent();
+	}
+	
+	public Student save(Student student) {
+        ResponseEntity<Student> exchangePost = restTemplateAdmin.exchange("/",
+                HttpMethod.POST, new HttpEntity<>(student, createJsonHeader()), Student.class);
+        return exchangePost.getBody();
+    }
+	
+	private  HttpHeaders createJsonHeader() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		return headers;
+	}
+	
+}
