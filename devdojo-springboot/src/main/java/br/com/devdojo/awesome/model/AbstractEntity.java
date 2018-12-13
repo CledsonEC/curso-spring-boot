599af130f72466bfb6cf4f71c4bbced39d98e59a
Cledson <cledson.caetano@gmail.com>
2018-12-13 20:17:19 -0200

    Aula 27 - Crindo teste de integração (endpoint)

--- devdojo-springboot/src/main/java/br/com/devdojo/awesome/model/AbstractEntity.java
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/model/AbstractEntity.java
@@ -13,7 +13,7 @@ public class AbstractEntity implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false, unique = true)
-	private Long id;
+	protected Long id;
 	 
 	@Override
 	public boolean equals(Object obj) {
